56e3bc11bc423df88ecdd81eb59092b15366dac7
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-07-01 15:00:36 -0300

    [change] assert

---
+++ controle_de_fluxo/julia.oliveira/assert.dart
@@ -0,0 +1,17 @@
+void main(){
+  // Animal animal = Animal ('Totó', 100);
+  Carro carro = Carro ('corola', 3);
+}
+// class Animal{
+//   // String nome;
+//   // int idade;
+
+//   // Animal(this.nome, this.idade) : assert (idade <= 20, 'idade inválida'); //Só funciona no debug para descobrir erros
+// }
+
+class Carro{
+  String nome;
+  int rodas;
+
+  Carro(this.nome, this. rodas) : assert (rodas == 4, 'Isso é um carro ');
+}
